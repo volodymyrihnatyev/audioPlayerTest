62f8d70887cdbc8f3bca46cc9f311e3cd4a3a7de
volodymyrignatiev <volodymyrignatiev94@gmail.com>
2016-04-06 13:55:21 +0300

    Encapsulate all logic in AudioPlayer class

--- js/main.js
+++ js/main.js
@@ -1,135 +1,151 @@
-var audio;
-var volume =0.5;
-$('#volume').val($('#volume').data("default"));
+(function(){
 
+var audioPlayer = new AudioPlayer();
+audioPlayer.init();
 
-//hide pause button
-$('#pause').hide();
+function AudioPlayer(){
+	var self = this;
+	var firtSong = $('#playlist li:first-child');
+	var lastSong = $('#playlist li:last-child');
 
-//Initialize first song
-initAudio($('#playlist li:first-child'));
+	this.audio = null;
+	var next = $('#next');
+	var prev = $('#prev');
+	var pause = $('#pause');
+	var stop = $('#stop');
+	var play = $('#play');
+	var volume = $('#volume');
+	var activeAudio = $('#playlist li.active');
+	var audioInList = $('#playlist li');
 
-//Initializer function
-function initAudio(elem){
-	var song = elem.attr('song');
-	var title = elem.text();
-	var cover = elem.attr('cover');
-	var artist = elem.attr('artist');
+	var volumeValue = volume.val() / 10;
 
-	//Create audio object
-	audio = new Audio('media/' + song);
+	this.init = function(){
+			//Set default volume value
+			volume.val($(this).data("default"));
+			//hide pause button
+			pause.hide();
+			//Initialize first song
+			self.initAudio(firtSong);
 
-	if (!audio.currentTime){
-		$('#duration').html('0.00');
-	}
+			//initialize events
+			play.click(self.actions.play);
+			pause.click(self.actions.pause);
+			stop.click(self.actions.stop);
+			next.click(self.actions.next);
+			prev.click(self.actions.prev);
+			volume.change(self.actions.volumeChange);
+			audioInList.click(self.actions.playAudioInList);
+	};
 
-	$('#audio-player .title').text(title);
-	$('#audio-player .artist').text(artist);
+	this.initAudio = function(elem){
+		var song = elem.attr('song');
+		var title = elem.text();
+		var cover = elem.attr('cover');
+		var artist = elem.attr('artist');
 
-	//Insert cover image
-	$('img.cover').attr('src', 'img/covers/' + cover);
+		//Create audio object
+		self.audio = new Audio('media/' + song);
 
-	$('#playlist li.active').removeClass('active');
-	elem.addClass('active');
-};
-
-function togglePlayButton(){
-	if (!audio.paused){
-		$('#play').hide();
-		$('#pause').show();
-	} else {
-		$('#pause').hide();
-		$('#play').show();
-	}
-};
-
-//Play button
-$('#play').click(function(){
-	audio.volume = volume;
-	audio.play();
-	togglePlayButton();
-	showDuration();
-});
-
-//Pause button
-$('#pause').click(function(){
-	audio.pause();
-	togglePlayButton();
-});
-
-//Stop Button
-$('#stop').click(function(){
-	audio.pause();
-	audio.currentTime = 0;
-	togglePlayButton();
-});
-
-//Next Button
-$('#next').click(function(){
-	audio.pause();
-	var next = $('#playlist li.active').next();
-	if(next.length ==0){
-		next = $('#playlist li:first-child')
-	}
-	initAudio(next);
-	audio.volume = volume;
-	// if (!audio.paused){
-
-	// }
-	audio.play();
-	togglePlayButton();
-	showDuration();
-});
-
-//Prev Button
-$('#prev').click(function(){
-	audio.pause();
-	var prev = $('#playlist li.active').prev();
-	if(prev.length ==0){
-		prev = $('#playlist li:last-child')
-	}
-	initAudio(prev);
-	audio.volume = volume;
-	audio.play();
-	togglePlayButton();
-	showDuration();
-});
-
-//Click on list item
-$('#playlist li').click(function(){
-	audio.pause();
-	initAudio($(this));
-	audio.volume = volume;
-	audio.play();
-	togglePlayButton();
-	showDuration();
-});
-
-//Volume Control
-$('#volume').change(function(){
-	volume = audio.volume = this.value / 10;
-});
-
-//Time Duration
-function showDuration(){
-	$(audio).on('timeupdate', function(){
-		//Get Hours & Minutes
-		var s = parseInt(audio.currentTime % 60);
-		var m = parseInt(audio.currentTime / 60) % 60;
-		//Add 0 if less than 10
-		if (s < 10){
-			s = '0' + s;
+		if (!self.audio.currentTime){
+			$('#duration').html('0.00');
 		}
-		$('#duration').html(m + ':' + s);
-		var value = 0;
-		if(audio.currentTime > 0){
-			value = Math.floor((100 / audio.duration) * audio.currentTime);
-		}
-		$('#progress').css('width', value + '%');
 
-		//play next song after current song have just played
-		if( audio.currentTime >= audio.duration) {
-			$('#next').trigger('click');
-		}﻿
-	});
-};
+		$('#audio-player .title').text(title);
+		$('#audio-player .artist').text(artist);
+
+		//Insert cover image
+		$('img.cover').attr('src', 'img/covers/' + cover);
+
+		activeAudio.removeClass('active');
+		activeAudio = $(elem);
+		elem.addClass('active');
+	};
+	this.actions = {
+		next: function(){
+			self.audio.pause();
+			var next = activeAudio.next();
+			if(next.length ==0){
+				next = firtSong;
+			}
+			self.initAudio(next);
+			self.audio.volume = volumeValue;
+			// if (!audio.paused){
+
+			// }
+			self.audio.play();
+			self.actions.togglePlayButton();
+			self.actions.showDuration();
+		},
+		prev: function(){
+			self.audio.pause();
+			var prev = activeAudio.prev();
+			if(prev.length ==0){
+				prev = lastSong;
+			}
+			self.initAudio(prev);
+			self.audio.volume = volumeValue;
+			self.audio.play();
+			self.actions.togglePlayButton();
+			self.actions.showDuration();
+		},
+		play: function(){
+				self.audio.volume = volumeValue;
+				self.audio.play();
+				self.actions.togglePlayButton();
+				self.actions.showDuration();
+		},
+		stop: function(){
+				self.audio.pause();
+				self.audio.currentTime = 0;
+				self.actions.togglePlayButton();
+		},
+		pause: function(){
+				self.audio.pause();
+				self.actions.togglePlayButton();
+		},
+		playAudioInList: function(){
+				self.audio.pause();
+				self.initAudio($(this));
+				self.audio.volume = volumeValue;
+				self.audio.play();
+				self.actions.togglePlayButton();
+				self.actions.showDuration();
+		},
+		volumeChange: function(){
+				volumeValue = self.audio.volume = this.value / 10;
+		},
+		togglePlayButton: function (){
+				if (!self.audio.paused){
+					play.hide();
+					pause.show();
+				} else {
+					pause.hide();
+					play.show();
+				}
+		},
+		showDuration: function(){
+			$(self.audio).on('timeupdate', function(){
+				//Get Hours & Minutes
+				var s = parseInt(self.audio.currentTime % 60);
+				var m = parseInt(self.audio.currentTime / 60) % 60;
+				//Add 0 if less than 10
+				if (s < 10){
+					s = '0' + s;
+				}
+				$('#duration').html(m + ':' + s);
+				var value = 0;
+				if(self.audio.currentTime > 0){
+					value = Math.floor((100 / self.audio.duration) *self.audio.currentTime);
+				}
+				$('#progress').css('width', value + '%');
+
+				//play next song after current song have just played
+				if( self.audio.currentTime >= self.audio.duration) {
+					next.trigger('click');
+				}﻿
+			});
+		}
+		};
+	};
+})();
